7fd92a6968b2fb343ed69b15e503fcd82b190fef
Matias Insaurralde <matias@insaurral.de>
2015-11-23 11:29:30 -0300

    socket.send

--- soocoin/client.js
+++ soocoin/client.js
@@ -3,14 +3,25 @@ var fs = require( 'fs' ),
     crypto = require( 'crypto' ),
     bs58 = require( 'bs58' ),
     EventEmitter = require( 'events' ),
-    NodeRSA = require( 'node-rsa' );
+    NodeRSA = require( 'node-rsa' ),
+    dgram = require( 'dgram' );
 
 var Soocoin = {};
 
 Soocoin.client = function( params ) {
-  var self = this;
+  var self = this,
+      socket;
+
   self.send = function( tx ) {
     console.info( '(client) sending tx', tx );
+    socket = dgram.createSocket( 'udp4' );
+    socket.send( tx, 0, tx.length, 7777, '127.0.0.1', function( err ) {
+      console.info( '(client) err?', err );
+      socket.close();
+    });
+  };
+  self.readBlockchain = function() {
+    
   };
 };
 
