3598b33009f84d1420ecbc15a3be638d273a4ae7
Matias Insaurralde <matias@insaurral.de>
2015-11-23 11:30:03 -0300

    variacion de tx

--- send.js
+++ send.js
@@ -7,7 +7,9 @@ wallet.on( 'ready', function() {
   console.info( '(wallet) ready' );
   console.info( '(wallet) address', wallet.getAddress() );
 
-  wallet.send( 100000000, '7xjySUAgyzre2b41PDqKByj9fbknRLvxJ', function( tx ) {
+//   wallet.send( 100000000, '7xjySUAgyzre2b41PDqKByj9fbknRLvxJ', function( tx ) {
+  wallet.send( 1, '7xjySUAgyzre2b41PDqKByj9fbknRxyxy', function( tx ) {
     client.send( tx );
   });
+
 });
