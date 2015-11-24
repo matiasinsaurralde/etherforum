f661285a4e58ef4cb3a2e96d777eecc6f359593b
Matias Insaurralde <matias@insaurral.de>
2015-11-24 06:49:40 -0300

    ecdh tests

--- x.js
+++ x.js
@@ -1,6 +1,30 @@
-var createKeys = require('rsa-json'),
-    fs = require( 'fs' );
+var crypto = require('crypto');
 
-createKeys( {}, function( a, b ) {
-  fs.writeFile( 'god_keys', JSON.stringify(b) );
-});
+var alice = crypto.createECDH('secp256k1');
+var bob = crypto.createECDH('secp256k1');
+
+alice.generateKeys();
+bob.generateKeys();
+
+console.log( 'alice private key', alice.getPrivateKey() );
+console.log( 'alice pubkey', alice.getPublicKey() );
+
+var signature = crypto.createSign( 'RSA-SHA256' );
+
+var message = new Buffer('el mensaje' );
+console.log( 'message', message );
+signature.update( message );
+
+console.log( 'privkey', alice.getPrivateKey().toString('base64') );
+// console.log( 'signature', signature.sign('hex') );
+
+// var alice_secret = alice.computeSecret(bob.getPublicKey(), null, 'hex');
+// var bob_secret = bob.computeSecret(alice.getPublicKey(), null, 'hex');
+
+
+// alice.generateKeys();
+// bob.generateKeys();
+
+// var alice_secret = alice.computeSecret(bob.getPublicKey(), null, 'hex');
+// var bob_secret = bob.computeSecret(alice.getPublicKey(), null, 'hex');
+
