e9f075eb09002fb0ab6987e7f00d2d938f1b5f0b
Matias Insaurralde <matias@insaurral.de>
2015-11-23 21:05:11 -0300

    otro tipo de tx

--- send.js
+++ send.js
@@ -1,15 +1,18 @@
 var Soocoin = require( './soocoin' );
 
 var wallet = new Soocoin.wallet( { keys: 'wallet' } ),
-    client = new Soocoin.client();
+    client = new Soocoin.client(wallet);
 
 wallet.on( 'ready', function() {
   console.info( '(wallet) ready' );
   console.info( '(wallet) address', wallet.getAddress() );
 
 //   wallet.send( 100000000, '7xjySUAgyzre2b41PDqKByj9fbknRLvxJ', function( tx ) {
+/*
   wallet.send( 1, '7xjySUAgyzre2b41PDqKByj9fbknRxyxy', function( tx ) {
     client.send( tx );
   });
+*/
+//  client.create_forum( 'un foro de prueba' );
 
 });
