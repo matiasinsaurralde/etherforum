ac37732d70a04525de410a90c1e5c1130fd64c1d
Matias Insaurralde <matias@insaurral.de>
2015-11-23 21:05:37 -0300

    tx

--- soocoin/client.js
+++ soocoin/client.js
@@ -6,11 +6,17 @@ var fs = require( 'fs' ),
     NodeRSA = require( 'node-rsa' ),
     dgram = require( 'dgram' );
 
+const costs = {
+  forum_creation: 10000
+};
+
 var Soocoin = {};
 
-Soocoin.client = function( params ) {
+Soocoin.client = function( wallet ) {
   var self = this,
       socket;
+
+  self.wallet = wallet;
 
   self.send = function( tx ) {
     console.info( '(client) sending tx', tx );
@@ -20,8 +26,20 @@ Soocoin.client = function( params ) {
       socket.close();
     });
   };
+
+  self.create_forum = function( forumName ) {
+    console.info( '(client) create_forum', forumName );
+    var tx = { input: self.wallet.getAddress(), forum_name: 'un foro de prueba' },
+        signedTx = self.wallet.sign( tx );
+
+    socket = dgram.createSocket( 'udp4' );
+    socket.send( signedTx, 0, signedTx.length, 7777, '127.0.0.1', function( err ) {
+      console.info( '(client) err?', err );
+      socket.close();
+    });
+  };
+
   self.readBlockchain = function() {
-    
   };
 };
 
