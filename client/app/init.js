0ac50110a803eb419462ed22a016f91df085f250
Matias Insaurralde <matias@insaurral.de>
2015-12-14 06:02:43 -0300

    stuff 👽

--- client/app/init.js
+++ client/app/init.js
@@ -15,3 +15,17 @@ config.$inject = ['$urlRouterProvider', '$locationProvider' ];
 
     $locationProvider.hashPrefix('!');
   }
+
+window.onload = function() {
+
+  window.web3 = new Web3(); // ??
+
+  window.forumContract = 0xfc7575c74363e9e554e6b0a6b7f43f740010fb1e;
+  window.forumAbiDefinition = [{"constant":false,"inputs":[{"name":"title","type":"string"},{"name":"content","type":"string"},{"name":"forum","type":"uint256"}],"name":"createMessage","outputs":[{"name":"messageID","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"forums","outputs":[{"name":"owner","type":"address"},{"name":"name","type":"string"},{"name":"messageCount","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"}],"name":"createForum","outputs":[{"name":"forumID","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"forum","type":"uint256"},{"name":"message","type":"uint256"}],"name":"getMessage","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"forumCount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"title","type":"string"},{"indexed":false,"name":"content","type":"string"}],"name":"readMessage","type":"event"}];
+
+  var contract = web3.eth.contract( forumAbiDefinition ).at( forumContract );
+  web3.setProvider( new web3.providers.HttpProvider() );
+
+  document.getElementById( 'eth_coinbase' ).innerText = web3.eth.coinbase;
+
+};
