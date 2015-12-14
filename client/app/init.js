b65c9b8a18138d63e44a081f487f5fdbe64a667d
Matias Insaurralde <matias@insaurral.de>
2015-12-14 10:08:13 -0300

    default contract stuff

--- client/app/init.js
+++ client/app/init.js
@@ -1,5 +1,6 @@
 angular.module('cryptoforum', [ 'ngAnimate', 'ui.router', 'anim-in-out' ])
 .controller( 'HomeCtrl', HomeCtrl )
+.controller( 'ForumCtrl', [ '$rootScope', '$state', ForumCtrl ] )
 .config( config )
 .config( routes )
 .run( [ '$rootScope', function( $rootScope ) {
@@ -9,6 +10,13 @@ angular.module('cryptoforum', [ 'ngAnimate', 'ui.router', 'anim-in-out' ])
   window.web3 = web3;
 
   // $rootScope.$watch( $rootScope.web3.eth.getBalance(web3.eth.coinbase).c ) // :p
+
+  $rootScope.defaultContract = { id: '0x0b092122102eeb28b1980488f8335617c702d1d8',
+abi: [{"constant":false,"inputs":[{"name":"title","type":"string"},{"name":"content","type":"string"},{"name":"forum","type":"uint256"}],"name":"createMessage","outputs":[{"name":"messageID","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"forums","outputs":[{"name":"owner","type":"address"},{"name":"name","type":"string"},{"name":"messageCount","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"}],"name":"createForum","outputs":[{"name":"forumID","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"forum","type":"uint256"},{"name":"message","type":"uint256"}],"name":"getMessage","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"forumCount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"title","type":"string"},{"indexed":false,"name":"content","type":"string"}],"name":"readMessage","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"name","type":"string"},{"indexed":false,"name":"forumID","type":"uint256"}],"name":"forumCreated","type":"event"}] }
+
+  $rootScope.contract =  web3.eth.contract( $rootScope.defaultContract.abi ).at( $rootScope.defaultContract.id );
+  window.contract = $rootScope.contract;
+
 }]);
 
 config.$inject = ['$urlRouterProvider', '$locationProvider' ];
