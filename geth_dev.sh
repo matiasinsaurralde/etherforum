3cd02a9d741d8228d6e1941f837d47c7115f6beb
Matias Insaurralde <matias@insaurral.de>
2015-12-14 10:09:07 -0300

    modifying my etherbase

--- geth_dev.sh
+++ geth_dev.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-geth --dev --datadir ~/ethereum_dev --mine --minerthreads 8 --etherbase e140d3dbccc7353b754fe35165a743edd95ce808 --maxpeers 25 --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpccorsdomain "http://localhost"
+geth --dev --datadir ~/ethereum_dev --mine --minerthreads 8 --etherbase 5d2da6c536f5aaf1b05ea5eeced388c602280365 --maxpeers 25 --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpccorsdomain "http://localhost"
