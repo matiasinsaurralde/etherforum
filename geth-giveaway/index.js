e058110bab9403599c478a7dc098a3c050e9a543
Matias Insaurralde <matias@insaurral.de>
2015-12-15 05:49:38 -0300

    giveaway tuneup (exposing POST)

--- geth-giveaway/index.js
+++ geth-giveaway/index.js
@@ -1,9 +1,12 @@
 var Web3 = require( 'web3' ),
-    web3 = new Web3()
+    express = require( 'express' ),
+    bodyParser = require('body-parser')
+    web3 = new Web3(),
+    app = express()
 
 web3.setProvider(new web3.providers.HttpProvider( 'http://localhost:8545' ));
 
-var giveAwayContract = web3.eth.contract( [{"constant":false,"inputs":[],"name":"registerAddress","outputs":[{"name":"addressID","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"addressCount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"addresses","outputs":[{"name":"","type":"address"}],"type":"function"}] ).at( '0x20fa4d3e39897e12045d9b52827a991d352a912e' )
+var giveAwayContract = web3.eth.contract( [{"constant":false,"inputs":[],"name":"registerAddress","outputs":[{"name":"addressID","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"foreignAddress","type":"address"}],"name":"registerForeignAddress","outputs":[{"name":"addressID","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"addressCount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"addresses","outputs":[{"name":"","type":"address"}],"type":"function"}] ).at('0x437cd5726c971c22860a8218b5fcd6ad0f7f40d8');
 
 function sendEther( to, value, gasPrice, gas, callback ) {
   web3.eth.sendTransaction({
@@ -33,6 +36,18 @@ function giveaway() {
   };
 };
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post( '/appendAddress', function( req, res ) {
+  var addr = req.body.address;
+  console.log( '** Appending address', addr );
+  giveAwayContract.registerForeignAddress( '0x0000000000000000000000000000000000000123', { from: web3.eth.coinbase } );
+  res.write('');
+  res.end();
+});
+
 giveaway();
 
-setInterval( giveaway, 30000 );
+setInterval( giveaway, 60000 );
+
+app.listen( 8540 )
