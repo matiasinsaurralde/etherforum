edbe2bb4caebdb52a38d5de1a81de2a9ef9fd11c
Matias Insaurralde <matias@insaurral.de>
2015-12-15 00:39:37 -0300

    cool abstractions

--- client/app/init.js
+++ client/app/init.js
@@ -1,21 +1,27 @@
 angular.module('cryptoforum', [ 'ngAnimate', 'ui.router', 'anim-in-out' ])
-.controller( 'HomeCtrl', HomeCtrl )
+.controller( 'HomeCtrl', [ '$scope', 'walletService', HomeCtrl ] )
 .controller( 'ForumCtrl', [ '$rootScope', '$state', ForumCtrl ] )
+.service( 'walletService', WalletService )
 .config( config )
 .config( routes )
-.run( [ '$rootScope', function( $rootScope ) {
+.run( [ '$rootScope', '$state', 'walletService', function( $rootScope, $state, walletService ) {
   var web3 = new Web3();
-  web3.setProvider( new web3.providers.HttpProvider() );
+
+  // web3.setProvider( new web3.providers.HttpProvider() );
   $rootScope.web3 = web3;
   window.web3 = web3;
 
   // $rootScope.$watch( $rootScope.web3.eth.getBalance(web3.eth.coinbase).c ) // :p
 
   $rootScope.defaultContract = { id: '0x0b092122102eeb28b1980488f8335617c702d1d8',
-abi: [{"constant":false,"inputs":[{"name":"title","type":"string"},{"name":"content","type":"string"},{"name":"forum","type":"uint256"}],"name":"createMessage","outputs":[{"name":"messageID","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"forums","outputs":[{"name":"owner","type":"address"},{"name":"name","type":"string"},{"name":"messageCount","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"}],"name":"createForum","outputs":[{"name":"forumID","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"forum","type":"uint256"},{"name":"message","type":"uint256"}],"name":"getMessage","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"forumCount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"title","type":"string"},{"indexed":false,"name":"content","type":"string"}],"name":"readMessage","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"name","type":"string"},{"indexed":false,"name":"forumID","type":"uint256"}],"name":"forumCreated","type":"event"}] }
+    abi: [{"constant":false,"inputs":[{"name":"title","type":"string"},{"name":"content","type":"string"},{"name":"forum","type":"uint256"}],"name":"createMessage","outputs":[{"name":"messageID","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"forums","outputs":[{"name":"owner","type":"address"},{"name":"name","type":"string"},{"name":"messageCount","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"}],"name":"createForum","outputs":[{"name":"forumID","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"forum","type":"uint256"},{"name":"message","type":"uint256"}],"name":"getMessage","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"forumCount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"title","type":"string"},{"indexed":false,"name":"content","type":"string"}],"name":"readMessage","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"name","type":"string"},{"indexed":false,"name":"forumID","type":"uint256"}],"name":"forumCreated","type":"event"}] }
 
   $rootScope.contract =  web3.eth.contract( $rootScope.defaultContract.abi ).at( $rootScope.defaultContract.id );
   window.contract = $rootScope.contract;
+
+  if( walletService.storedWalletAvailable() ) {
+    var storedWallet = walletService.storedWalletRestore();
+  };
 
 }]);
 
