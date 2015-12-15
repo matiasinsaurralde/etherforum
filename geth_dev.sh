16495bdf0591b234dee9d2490780d36e88534fff
Matias Insaurralde <matias@insaurral.de>
2015-12-15 05:52:07 -0300

    appending unlock arg to geth

--- geth_dev.sh
+++ geth_dev.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-geth --dev --datadir ~/ethereum_dev --mine --minerthreads 8 --etherbase 5d2da6c536f5aaf1b05ea5eeced388c602280365 --maxpeers 25 --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpccorsdomain "http://localhost"
+geth --dev --datadir ~/ethereum_dev --mine --minerthreads 1 --etherbase 5d2da6c536f5aaf1b05ea5eeced388c602280365 --maxpeers 25 --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpccorsdomain "http://192.168.0.3" --unlock 0x5d2da6c536f5aaf1b05ea5eeced388c602280365
