28e0508ffc53eaf178fbe739e5e2119397ea119b
Matias Insaurralde <matias@insaurral.de>
2015-12-15 07:11:44 -0300

    implementing destroyAddressAndKeys, just in case

--- client/app/controllers/home.js
+++ client/app/controllers/home.js
@@ -1,5 +1,10 @@
-function HomeCtrl( $scope, walletService ) {
+function HomeCtrl( $scope, $window, $state, walletService ) {
   $scope.generateAddress = function() {
     walletService.generateAddress();
+  };
+
+  $scope.destroyAddressAndKeys = function() {
+    $window.localStorage.clear();
+    $window.location.reload();
   };
 };
